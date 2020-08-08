76512f4a19595cd7c6b6827dae5a0bd12f5fef48
Mehmet <myesil3445@gmail.com>
2020-08-08 22:55:40 +0200

    deneme2

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("MERHABA");
         System.out.println("MERHABA");
-        
+        System.out.println("DENEME");
     }
 }
